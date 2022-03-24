567ce6bdcd20eda0d5405ade325848449d8fe10e
Philip Atkinson <philip.atkinson@unrationalgames.com>
2022-03-24 18:36:55 +0000

    Update Types and Related TSDocs

--- src/types.ts
+++ src/types.ts
@@ -1,43 +1,51 @@
 export type CacheControl = {
-  browserTTL: number
-  edgeTTL: number
-  bypassCache: boolean
-}
-
-export type AssetManifestType = Record<string, string>
-
-export type Options = {
-  cacheControl: ((req: Request) => Partial<CacheControl>) | Partial<CacheControl>
-  ASSET_NAMESPACE: any
-  ASSET_MANIFEST: AssetManifestType | string
-  mapRequestToAsset?: (req: Request, options?: Partial<Options>) => Request
-  defaultMimeType: string
-  defaultDocument: string
-  pathIsEncoded: boolean
-}
-
-export class KVError extends Error {
-  constructor(message?: string, status: number = 500) {
-    super(message)
-    // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
-    Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
-    this.name = KVError.name // stack traces display correctly now
-    this.status = status
+	browserTTL: number
+	bypassCache: boolean
+	edgeTTL: number
   }
-  status: number
-}
-export class MethodNotAllowedError extends KVError {
-  constructor(message: string = `Not a valid request method`, status: number = 405) {
-    super(message, status)
+  
+  export type AssetManifestType = Record<string, string>
+  
+  export type Evt = {
+	request: Request
+	waitUntil: (promise: Promise<any>) => void
   }
-}
-export class NotFoundError extends KVError {
-  constructor(message: string = `Not Found`, status: number = 404) {
-    super(message, status)
+  
+  export type Options = {
+	ASSET_NAMESPACE: KVNamespace
+	ASSET_MANIFEST: AssetManifestType | string
+	cacheControl: ((req: Request) => Partial<CacheControl>) | Partial<CacheControl>
+	defaultMimeType: string
+	defaultDocument: string
+	mapRequestToAsset?: (req: Request, options?: Partial<Options>) => Request
+	pathIsEncoded: boolean
   }
-}
-export class InternalError extends KVError {
-  constructor(message: string = `Internal Error in KV Asset Handler`, status: number = 500) {
-    super(message, status)
+  
+  export class KVError extends Error {
+	constructor(message?: string, status: number = 500) {
+	  super(message)
+	  // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
+	  Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
+	  this.name = KVError.name // stack traces display correctly now
+	  this.status = status
+	}
+	status: number
   }
-}
+  
+  export class MethodNotAllowedError extends KVError {
+	constructor(message: string = `Not a valid request method`, status: number = 405) {
+	  super(message, status)
+	}
+  }
+  
+  export class NotFoundError extends KVError {
+	constructor(message: string = `Not Found`, status: number = 404) {
+	  super(message, status)
+	}
+  }
+  
+  export class InternalError extends KVError {
+	constructor(message: string = `Internal Error in KV Asset Handler`, status: number = 500) {
+	  super(message, status)
+	}
+  }  
